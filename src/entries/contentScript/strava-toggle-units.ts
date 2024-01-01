d80fa6e357833d99d5459f4ed9b5fd89583dfa13
Andrew Hudson <ahudson@gmail.com>
2024-01-01 16:53:54 +0100

    fix ts-error

--- src/entries/contentScript/strava-toggle-units.ts
+++ src/entries/contentScript/strava-toggle-units.ts
@@ -3,19 +3,23 @@ import { messages } from "../consts/messages";
 
 document.addEventListener("click", async (e) => {
   console.log("clicked");
-  if (!e.target) {
+  const { target } = e;
+
+  if (!target) {
     return;
   }
-  // check if item is a abbr.unit or the parent of abbr.unit
-  if (
-    (e.target.tagName === "ABBR" && e.target.classList.contains("unit")) ||
-    e.target.querySelector("abbr.unit")
-  ) {
-    console.log("is an abbr.unit, or parent of one");
+  if (target instanceof HTMLElement) {
+    // check if item is a abbr.unit or the parent of abbr.unit
+    if (
+      (target.tagName === "ABBR" && target.classList.contains("unit")) ||
+      target.querySelector("abbr.unit")
+    ) {
+      console.log("is an abbr.unit, or parent of one");
 
-    await browser.runtime.sendMessage({
-      message: messages.UNIT_TOGGLED,
-    });
+      await browser.runtime.sendMessage({
+        message: messages.UNIT_TOGGLED,
+      });
+    }
   }
 });
 
